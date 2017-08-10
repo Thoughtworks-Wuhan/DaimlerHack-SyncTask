356dd04d3f42b80f9062b60ae6244e9af863e4ad
JiangXu <jiangxu@thoughtworks.com>
2017-08-10 09:51:39 +0800

    fix conflict

--- src/main/java/com/daimler/Utils/CSV.java
+++ src/main/java/com/daimler/Utils/CSV.java
@@ -8,27 +8,6 @@ import java.io.*;
 
 @Service
 public class CSV {
-
-    public static void generateCSVFile(String fileName) throws FileNotFoundException {
-        PrintWriter pw = new PrintWriter(new File(fileName));
-        StringBuilder sb = new StringBuilder();
-        sb.append("road_haul");
-        sb.append(',');
-        sb.append("new_price");
-        sb.append(',');
-//        sb.append("title");
-//        sb.append(',');
-//        sb.append("use_date");
-//        sb.append(',');
-//        sb.append("insurance_date");
-//        sb.append(',');
-//        sb.append("gear_box");
-//        sb.append(',');
-        sb.append("price");
-        sb.append('\n');
-        pw.close();
-    }
-
     public static String replaceCommaWithSpace(String field) {
         if (field != null) {
             return field.replace(',', ' ').replace("\n", " ");
